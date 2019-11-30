c7381b61767aa6296e598844e761c27c94afee33
aaaaaaaaaaaaaaaaaaaaaa <aaaaaaaaaaaaaaaaaaaaaaaaaaaaa>
2019-11-30 15:29:48 +0900

    入力システム完成

--- Action/Game.cpp
+++ Action/Game.cpp
@@ -44,7 +44,7 @@ Game::~Game()
 bool Game::Initialize()
 {
 	//SDLÇÃèâä˙âª
-	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0)
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO | SDL_INIT_GAMECONTROLLER) != 0)
 	{
 		SDL_Log("Unable to initialize SDL: %s", SDL_GetError());
 		return false;
