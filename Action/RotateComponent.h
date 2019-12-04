d72e68036c4a123d04cdaaaec7caca8e286c8ea4
kazuya <o0kazuya.orimoto0o@gmail.com>
2019-12-04 13:54:22 +0900

    コメントを追加

--- Action/RotateComponent.h
+++ Action/RotateComponent.h
@@ -2,6 +2,10 @@
 #include "Component.h"
 #include "Math.h"
 
+/*
+ @file RotateComponent.h
+ @brief ŠÖ”‚É‰ñ“]‚³‚¹‚½‚¢Šp“x‚ğ“n‚·‚ÆeƒIƒuƒWƒFƒNƒg‚ğ‰ñ“]‚³‚¹‚é
+	*/
 class RotateComponent :
 	public Component
 {
@@ -9,6 +13,11 @@ public:
 	RotateComponent(GameObject* _owner, int _updateOrder=100);
 	~RotateComponent();
 
+	/*
+	@brief ‰ñ“]‚ğs‚¤
+	@param _rotate Šp“x
+	@param _axis ‰ñ“]²
+	*/
 	void SetRotation(const float& _rotate,const Vector3& _axis);
 };
 
